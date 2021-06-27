fb2db4a093c8c60ecb904358227ee5fc55f6bf5f
weilyuwang <weilyuwang@gmail.com>
2021-06-26 23:59:08 -0400

    enable CORS middleware

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -45,3 +45,19 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
+}
