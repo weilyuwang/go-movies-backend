8276ebc0297aa7622f690bde5948bf5c021a610b
weilyuwang <weilyuwang@gmail.com>
2021-08-04 00:11:50 -0400

    delete movie

--- models/movies-db.go
+++ models/movies-db.go
@@ -222,3 +222,14 @@ func (m *DBModel) UpdateMovie(movie Movie) error {
 
 	return err
 }
+
+func (m *DBModel) DeleteMovie(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `delete from movies where id = $1`
+
+	_, err := m.DB.ExecContext(ctx, stmt, id)
+
+	return err
+}
