eeafdecfbcf12bfa8370bc14e7c15383924158c2
weilyuwang <weilyuwang@gmail.com>
2021-08-09 22:44:53 -0400

    better error status code

--- cmd/api/utilities.go
+++ cmd/api/utilities.go
@@ -27,7 +27,13 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	return nil
 }
 
-func (app *application) errorJSON(w http.ResponseWriter, err error) {
+func (app *application) errorJSON(w http.ResponseWriter, err error, status ...int) {
+	// default code: 400
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
 	type jsonError struct {
 		Message string `json:"message"`
 	}
@@ -37,7 +43,7 @@ func (app *application) errorJSON(w http.ResponseWriter, err error) {
 	}
 
 	// Status Code 400 (Bad Request if Error)
-	app.writeJSON(w, http.StatusBadRequest, theError, "error")
+	app.writeJSON(w, statusCode, theError, "error")
 }
 
 func generateHashedPassword() string {
